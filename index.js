37b508fedddc7c13a3313bebf05214a1d3b793ee
Chempearcedev <chempearce1@hotmail.com>
2021-09-21 08:08:11 +0000

    Task function and test plan

---
+++ index.js
@@ -0,0 +1,15 @@
+var columns = 3;
+var rows = 3;
+
+var array = [];
+array[0] = {id:1};
+array[1] = {id:1};
+array [2] = {id:2};
+
+function test (array) {
+    for (var i = 0; i < columns; i++){
+      for (var j = 0; j < rows; j++) {
+      }  
+    }
+}
+console.log(array)
